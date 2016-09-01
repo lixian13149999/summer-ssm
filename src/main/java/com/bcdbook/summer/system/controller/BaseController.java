558763caa63fd8be2ba033dc89fa2c6c0c7d516d
lason <xianforwork@163.com>
2016-09-01 18:33:32 +0800

    解决github不能提交jade文件的问题

--- src/main/java/com/bcdbook/summer/system/controller/BaseController.java
+++ src/main/java/com/bcdbook/summer/system/controller/BaseController.java
@@ -1,5 +1,8 @@
 package com.bcdbook.summer.system.controller;
 
+import java.util.List;
+
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,14 +14,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bcdbook.summer.common.util.JadeUtil;
+import com.bcdbook.summer.system.pojo.Menu;
+import com.bcdbook.summer.system.service.MenuService;
 
 @Controller
 @RequestMapping("/")
 public class BaseController {
+	@Resource
+	private MenuService menuService;
 	
 	@RequestMapping(method = {RequestMethod.GET}) 
 	public ModelAndView getLoginPage(HttpServletRequest req,HttpServletResponse resp,Model model){
+		Menu menu = new Menu();
+		menu.setParentId("first");
+		List<Menu> userMenus = menuService.findList(menu);
+		
+		
 		ModelAndView mv = JadeUtil.getView("index.jade");
+		
 		return mv;
 	}
 	
