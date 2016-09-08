20c7f18f0a93635ae9ba62c750113d98bd545af5
lason <xianforwork@163.com>
2016-09-08 18:21:15 +0800

    加入栏目点击后的效果切换

---
+++ src/main/webapp/static/pc/js/common/imenu.js
@@ -0,0 +1,16 @@
+var imenu = new Object();
+$(function(){
+    imenu.clickMenu();
+})
+
+imenu.clickMenu = function(){
+    $(document).on('click','[data-page-click="menu-item"]',function(){
+//        console.log("menuClick");
+//        var parent = 
+        $(this).parents('[data-cont="menu-cont"]').find('[data-page-click="menu-item"]').each(function(index,ele){
+            $(ele).removeClass("activ");
+        })
+        $(this).addClass("activ");
+//        console.log(parent);
+    })
+}
