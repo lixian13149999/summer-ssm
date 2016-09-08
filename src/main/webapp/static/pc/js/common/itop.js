abe84622a6c61fed51eef779865bc908487597a6
lason <xianforwork@163.com>
2016-09-08 16:49:15 +0800

    加入了主页面中栏目部分的样式,重写了输入框样式

--- src/main/webapp/static/pc/js/common/itop.js
+++ src/main/webapp/static/pc/js/common/itop.js
@@ -2,12 +2,12 @@ var itop = new Object();
 //获取微信二维码的代码块
 itop.getWechatBarcode = function() {
 //    console.log("进入获取二维码方法");
-	return '<div class="wechat_2d_barcode" style="background-image: url(\'http://localhost:8899/img/wechat_2d_barcode.jpg\');"></div>'
+	return '<div class="wechat-2d-barcode" style="background-image: url(\'http://localhost:8899/img/wechat_2d_barcode.jpg\');"></div>'
 }
 
 //获取用户简介的代码块
 itop.getUserIntd = function() {
-	var user_datas = $('#user_info_msg_hide_datas');
+	var user_datas = $('#user_info_hide_datas');
 
 	var _id = user_datas.data('para-_id'); //用户的id
 	var username = user_datas.data('para-username'); //用户名
@@ -18,21 +18,21 @@ itop.getUserIntd = function() {
 	var phone = user_datas.data('para-phone'); //电话
 	var depart_remark = user_datas.data('para-depart_remark'); //部门
 
-	return '<div class="user_intd_box">' +
-		'<div class="intd_top">' +
-		'<div style="background-image: url(' + photo + ');" class="intd_photo"></div>' +
-		'<div class="intd_info">' +
-		'<p class="intd_info_msg">' + depart_remark + '</p>' +
-		'<p class="intd_info_msg">' + realname + '</p>' +
-		'<p class="intd_info_msg">' + email + '</p>' +
+	return '<div class="user-intd-box">' +
+		'<div class="intd-top">' +
+		'<div style="background-image: url(' + photo + ');" class="intd-photo"></div>' +
+		'<div class="intd-info">' +
+		'<p class="intd-info-msg">' + depart_remark + '</p>' +
+		'<p class="intd-info-msg">' + realname + '</p>' +
+		'<p class="intd-info-msg">' + email + '</p>' +
 		'</div>' +
-		'<div class="intd_top_icon"><a id="user_shezhi_button" class="inner_icon_tiny icon_msg"><i class="iconfont hover_default icon-class-shezhi"></i></a></div>' +
+		'<div class="intd-top-icon"><a id="user_shezhi_button" class="inner-icon-tiny"><i class="icon hover-default icon-tool"></i></a></div>' +
 		'</div>' +
-		'<div class="intd_body"></div>' +
-		'<div class="intd_foot">' +
-		'<a class="intd_foot_msg hover_default">发起申请</a>' +
-		'<a class="intd_foot_icon" href="/logout">' +
-		'<i class="iconfont hover_default icon-class-exit"></i>'
+		'<div class="intd-body"></div>' +
+		'<div class="intd-foot">' +
+		'<a class="intd-foot-msg hover-default">发起申请</a>' +
+		'<a class="intd-foot-icon" href="/logout">' +
+		'<i class="icon hover-default icon-exit"></i>'
 		// '<i data-para-_id=' + _id + ' class="iconfont hover_default icon-class-exit user_exit_button_tip"></i>'
 	'+</a></div>' +
 	'</div>'
