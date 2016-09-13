0ca34cf9beb888c98bc628779a7da928aeb55dcd
lixian13149999 <lixian13149999@163.com>
2016-09-13 08:58:52 +0800

    验证持续调整中

--- src/main/webapp/static/pc/js/demo/testValidate.js
+++ src/main/webapp/static/pc/js/demo/testValidate.js
@@ -1,52 +1,52 @@
-function formAction(){
+function formAction() {
     console.log("formAction");
 }
 //        $().ready(function() {
 //            console.log("页面校验执行了初始化");
 //            $("#signupForm").validate();
 //        });
-$(function(){
+$(function () {
     var testValidate = $("#signupForm").validate({
         debug: true, //调试模式取消submit的默认提交功能   
         //errorClass: "label.error", //默认为错误的样式类为：error   
         focusInvalid: false, //当为false时，验证无效时，没有焦点响应  
-        onkeyup: false,   
-        submitHandler: function(form){   //表单提交句柄,为一回调函数，带一个参数：form   
+        onkeyup: false,
+        submitHandler: function (form) { //表单提交句柄,为一回调函数，带一个参数：form   
             // alert("提交表单");   
             console.log(form);
-            form.submit();   //提交表单   
-        },   
-        errorPlacement:function(error, element) {
-        	console.log(error);
-        	console.log(element);
+            form.submit(); //提交表单   
         },
-        rules:{
-            username:{
-                required:true,
+        errorPlacement: function (error, element) {
+            console.log(error);
+            console.log(element);
+        },
+        rules: {
+            username: {
+                required: true,
                 remote: {
-                    url: "userIsExist",     //后台处理程序
-                    type: "get",               //数据发送方式
-                    dataType: "json",           //接受数据格式   
-                    data: {                     //要传递的数据
-                        username: function() {
+                    url: "userIsExist", //后台处理程序
+                    type: "get", //数据发送方式
+                    dataType: "json", //接受数据格式   
+                    data: { //要传递的数据
+                        username: function () {
                             return $("#username").val();
                         }
                     }
                 }
             },
-            password:{
-                required:true,
-                rangelength:[3,10]
+            password: {
+                required: true,
+                rangelength: [3, 10]
             }
         },
-        messages:{
-            username:{
-                required:"必填",
-                remote:"ajax验证出现问题"
+        messages: {
+            username: {
+                required: "必填",
+                remote: "ajax验证出现问题"
             },
-            password:{
+            password: {
                 required: "不能为空",
-                rangelength:"长度限制"
+                rangelength: "长度限制"
             }
         }
     });
