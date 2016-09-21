a83331eca15c2e289a12664b30fd7ce445fe5465
Cassie-Yu <cassie-yu@hotmail.com>
2016-09-21 17:48:23 +0800

    登录注册页面修改提交
    1、样式细节修改
    2、js控制代码风格统一

---
+++ src/main/webapp/static/mobile/js/common/ipopover.js
@@ -0,0 +1,31 @@
+var input = new Object();
+
+//验证用户名是否为邮箱
+input.checkUserName = function(){
+    var UserName = $("#username").val();
+    var isnull = v.isNull(UserName);
+    var isemail = v.isEmail(UserName);
+    return !isnull&&isemail ? true : false ;
+}
+//验证密码是否大于6位
+input.checkPassword = function(){
+    var PassWord = $("#password").val();
+    var isnull = v.isNull(PassWord);
+    var istooshort = v.shortThan(PassWord,6);
+    return isnull||istooshort ? false : true;
+}
+//验证密码是否一致
+input.checkEqual = function(){
+    var PassWord = $("#password").val();
+    var PassWordAgain = $("#password-again").val();
+    var isequal = v.isSame(PassWord,PassWordAgain);
+    return isequal;
+}
+//判断输入框是否都满足条件
+input.verfyDo = function(a,b,c){
+    if(a&b&c){
+        $('.mobile-button').removeAttr('disabled');
+    }else{
+        $('.mobile-button').attr('disabled',true);
+    }
+}
--- src/main/webapp/static/mobile/js/sign.js
+++ src/main/webapp/static/mobile/js/sign.js
@@ -1,26 +1,71 @@
-$(document).on('input propertychange','[data-input-box="input"]',function(){
-    var content = $(this).val();
-    if(content != '' || content != ' ' ){
-        $(this).siblings('.delete').removeClass('hide');
-        $(document).on('click touchstart','[data-text-delete="delete"]',function(){
-            $(this).siblings('.input-box').val('');
-            $(this).addClass('hide');
-            $('.mobile-button').attr('disabled',true);
-        })
-    }
-    else{
-        $(this).siblings('.delete').addClass('hide');
-    }
+$(function(){
+    sign.input();//输入框焦点获失时样式变化
+    sign.deleteIcon();//输入框清空按钮的变化及相关操作
+    sign.verify();//表单验证后提交按钮的样式变化
 })
 
-$(document).on('keyup','[data-input-box="input"]',function(){
-    var content = $('.input-box').length;
-    for(var i=0;i<content;i++){
-        if($('.input-box').eq(i).val()==''||$('.input-box').eq(i).val()==' '){
-            $('.mobile-button').attr('disabled',true);
-        }else{
-            $('.mobile-button').removeAttr('disabled');
+var sign = new Object();
+
+sign.input = function(){
+    $(document).on('focus','[data-input-box="input"]',function(){
+        $(this).parent('.input-block').addClass("border");
+    })
+    $(document).on('blur','[data-input-box="input"]',function(){
+        $(this).parent('.input-block').removeClass('border');
+    })
+}
+
+sign.deleteIcon = function(){
+//当输入框内容发生改变时，清空按钮的变化
+    $(document).on('input propertychange','[data-input-box="input"]',function(){
+        var content = $(this).val();
+        if(content != '' && content != ' ' ){
+            $(this).siblings('.delete').removeClass('hide');
+//清空按钮点击的时候，输入框变化
+            $(document).on('click touchstart','[data-text-delete="delete"]',function(){
+                $(this).siblings('.input-box').val('');
+                $(this).addClass('hide');
+            })
         }
-    }
-})
+        else{
+            $(this).siblings('.delete').addClass('hide');
+        }
+    })
+}
+
+
+sign.verify = function(){
+    $(document).on('input propertychange','[data-input-box="input"]',function(){
+        if($('#password-again').length>0){
+            input.verfyDo(input.checkUserName(),input.checkPassword(),input.checkEqual());
+        }else{
+            input.verfyDo(input.checkUserName(),input.checkPassword(),true);
+        }
+    })
+}
+
+//执行登录操作
+sign.signin = function() {
+	var user = iform.parse("signin_form");
+	var url = "http://localhost:8080";
+	$.ajax({
+		url: url,
+		type: 'POST',
+		dataType: "json",
+		data: user,
+		async: false,
+		success: function(data) {
+			console.log(data);
+			sign.signinSuccessBack(data);
+			//cb(data);
+		},
+		error: function() {
+			alert('密码或用户名错误')
+		}
+	});
+}
+
+
+
+
 
