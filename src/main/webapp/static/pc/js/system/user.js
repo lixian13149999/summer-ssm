88eda70a35bf049938f8fea8a59ea2fb59f9d3a1
e <cassie-yu@hotmail.com>
2016-09-23 14:59:15 +0800

    系统主页面body内容添加
    1、lanmu-magage.html
    2、user-list.html

--- src/main/webapp/static/pc/js/system/user.js
+++ src/main/webapp/static/pc/js/system/user.js
@@ -1,3 +1,38 @@
-$(document).on("click","#commission-nav-box li",function(){
-    $(this).addClass("active").siblings().removeClass("active");
+
+$(function(){
+    user.navChange();//点击顶部分页菜单
+    user.listMove();//移动按钮的显示隐藏、列表拖拽排序
 })
+
+var user = new Object();
+
+
+//点击顶部分页菜单
+user.navChange = function(){
+        $(document).on("click","#commission-nav-box li",function(){
+        $(this).addClass("active").siblings().removeClass("active");
+    })
+}
+//移动按钮的显示隐藏、列表拖拽排序
+user.listMove = function(){
+    $(document).on('click','[data-move-icon="move"]',function(){
+        if($('.move').hasClass('hide')){
+            $('.move').removeClass('hide');
+            $('.each-list').removeClass('nomal');
+            $('#sortable').sortable({
+                handle:'.icon-grip'
+            });
+        }else{
+            $('.move').addClass('hide');
+            $('.each-list').addClass('nomal');
+        }
+    })
+}
+
+
+
+
+
+
+
+                       
