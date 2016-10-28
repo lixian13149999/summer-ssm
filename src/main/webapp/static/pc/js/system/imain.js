e384ff04ba91bb3587a52ef08b787fcf0b028445
lason <xianforwork@163.com>
2016-10-28 17:32:15 +0800

    加入公用的打开右侧内容页面的方法
    加入公用的打开右侧内容页面的方法

--- src/main/webapp/static/pc/js/system/imain.js
+++ src/main/webapp/static/pc/js/system/imain.js
@@ -5,12 +5,15 @@
 var imain = new Object();
 
 $(function(){
-    //加入头部弹出框的监听方法
+    //加入头部弹出框的监听方法(微信/个人信息)
     ipopover.listenerPromptCont();
-    //加入beaut输入框的监听方法
+    //加入beaut输入框的监听方法(输入框中属性提示上移效果)
     iform.listenerFormControl();
     
     imain.changePageTitleStyle();
+
+    //监听加载页面的方法(通过栏目的点击,加载一个新的页面)
+    imain.openPage();
 })
 
 imain.changePageTitleStyle = function(){
@@ -20,6 +23,14 @@ imain.changePageTitleStyle = function(){
         });
         $(this).addClass('checked');
     });
+}
+
+imain.openPage = function () {
+    $(document).on('click','[data-menu-side="item"]',function () {
+        var href = $(this).data("body-mapping");
+        console.log(href);
+        console.log("打开页面");
+    })
 }
 
 imain.testAddMenuList = function () {
